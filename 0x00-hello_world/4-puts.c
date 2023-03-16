90fc1004fe6920900372f00905e3118555ea42ee
tsholofelo Makaudi <hillarymtm@gmail.com>
2023-03-16 06:36:19 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - this is the main function
  *
- * return:  always 0
+ * return: always 0
  */
 
 int main(void)
- {
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
- }  
+}  
