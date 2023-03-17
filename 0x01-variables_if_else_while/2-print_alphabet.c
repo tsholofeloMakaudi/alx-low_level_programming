3eb7c31a9d34d2e04744431a6c8288e3005f2bda
tsholofelo Makaudi <hillarymtm@gmail.com>
2023-03-17 06:43:20 -0700

    my commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+
+{
+	int r = 97;
+
+	while (r <= 122)
+	{
+		putchar(r);
+		r++;
+	}
+	putchar('\n');
+	return (0);
+}
