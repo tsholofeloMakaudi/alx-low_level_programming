51d8c75ffe48d57e0593b43e10dce882980a98cb
tsholofelo Makaudi <hillarymtm@gmail.com>
2023-03-21 06:19:54 -0700

    my commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int _islower - checkes for lowercase charecters
+ * @c: The charecters to be checked
+ * Return: 1 for lowercase charecters 0 for anything else
+ */
+
+int _islower(int c)
+
+{
+	while (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
