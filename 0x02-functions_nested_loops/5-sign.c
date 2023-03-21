d9a7d1e65f11c81a8a7953cb5fef73f221136279
tsholofelo Makaudi <hillarymtm@gmail.com>
2023-03-21 07:53:15 -0700

    my commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - Checks if a number is positive or negative
+ * @n: the number to check
+ * Return: + for positive, - for negative and 0 for zero
+ */
+int print_sign(int n)
+
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (0);
+	}	
+}
+
