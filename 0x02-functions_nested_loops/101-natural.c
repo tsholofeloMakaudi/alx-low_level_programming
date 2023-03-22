9141182ddba4c5d58923ac1c3836caee04e417a6
tsholofelo Makaudi <hillarymtm@gmail.com>
2023-03-22 09:40:04 -0700

    my commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the the sum of all multiples of 3 or 5 under 1024
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	int i, z = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+		i++;
+	}
+	printf("%d", z);
+	return (0);
+}
