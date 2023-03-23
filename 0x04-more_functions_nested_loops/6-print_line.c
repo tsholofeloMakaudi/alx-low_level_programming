5371be599ee5aaf02370c14a545fb3bf1f8cd296
tsholofelo Makaudi <hillarymtm@gmail.com>
2023-03-23 08:27:44 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ * @n: the number of lines to draw
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
