7993faee976dafdfc4ffc9337dfe661557014e64
tsholofelo Makaudi <hillarymtm@gmail.com>
2023-03-23 01:17:16 -0700

    my commit

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -4,6 +4,8 @@ void positive_or_negative(int i)
 {
 	if (i < 0)
 		printf("%d is negative\n", i);
-	else
+	else if (i > 0)
 		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
 }
