d506d021fc401d18332d23b0609609df192747bf
tsholofelo Makaudi <hillarymtm@gmail.com>
2023-03-27 08:52:07 -0700

    my commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,15 @@
 
 void rev_string(char *s)
 {
-	int len = 0, index = 0;
+	int l = 0, i = 0;
 	char tmp;
 
-	while (s[index]++)
-	len++;
-	for (index = len - 1; index >= len / 2; index--)
+	while (s[i++])
+	l++;
+	for (i = l - 1; i >= l / 2; i--)
 	{
-		tmp = s[index];
-		s[index] = s[len - index - 1];
-		s[len - index - 1] = tmp;
+		tmp = s[i];
+		s[i] = s[l - i - 1];
+		s[l - i - 1] = tmp;
 	}
 }
