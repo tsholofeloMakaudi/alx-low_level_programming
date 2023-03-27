8fb29370d4d56cbcfdce862e0c404603cacc0643
tsholofelo Makaudi <hillarymtm@gmail.com>
2023-03-27 09:06:34 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints the second charactor of a string
+ * @str: the tring to be treated
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
