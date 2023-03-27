06054f75ba8b56110d322b8f0c987b1a2813fe6f
tsholofelo Makaudi <hillarymtm@gmail.com>
2023-03-27 08:05:14 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * void _puts - write a function that prints a string the a new line
+ * @str: the string to be printed
+ * Return: nothing
+ */
+
+void  _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
