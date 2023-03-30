278007cb5f6fef138c3e51eb7615e948bac0a37e
tsholofelo Makaudi <hillarymtm@gmail.com>
2023-03-30 05:03:05 -0700

    my commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (src[index++])
 		srclen++;
-	for (index = 0; src[index++] && index < n; index++)
+	for (index = 0; src[index] && index < n; index++)
 		dest[index] = src[index];
 	for (index = srclen; index < n; index++)
 		dest[index] = '\0';
