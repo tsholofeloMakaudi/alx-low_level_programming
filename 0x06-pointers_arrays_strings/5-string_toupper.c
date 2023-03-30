db92c895af2a4cdf8ef5119d0612658a78c696af
tsholofelo Makaudi <hillarymtm@gmail.com>
2023-03-30 05:48:37 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *string_toupper - changes all lower cases of a string to upper
+ * @str - the string to be checked
+ * Retuen: a pointer to the changed string
+ */
+
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+		index++;
+	}
+	return (str);
+}
