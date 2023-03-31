3925bebd525b6aded20163e0380c1e321ec4d885
tsholofelo Makaudi <hillarymtm@gmail.com>
2023-03-31 02:01:03 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *rot13 - encodes a string to rod13
+ * @s: the string to be encoded
+ * Return: the value of s
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
